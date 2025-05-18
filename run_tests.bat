639f45e153e7f6b4b6277a14e5a659252f4347d6
mohammed altaher <mohammedaltaher1254@gmail.com>
2025-05-18 13:31:43 +0400

    refactor: Remove deprecated test scripts and unused context/hooks files

--- run_tests.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Running tests with Vitest...
-cd %~dp0
-npm test
-echo.
-echo To run tests in watch mode, use: npm run test:watch
-echo To generate a coverage report, use: npm run test:coverage
